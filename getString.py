30311548858ca2867e6247d832c836b99da2541f
anieto1 <anieto1@binghamton.edu>
2016-02-25 19:04:12 -0500

    Final Project

---
+++ getString.py
@@ -0,0 +1,31 @@
+import getImage
+
+def getSearchValues(indexValues, textBox):
+    searchTerms = []
+    for values in indexValues:
+        splitValueList = values.split(',')
+        value1 = int(splitValueList[0])
+        value2 = int(splitValueList[1])
+        result = textBox[value1 + 1:value2]
+        searchTerms.append(result)
+    return searchTerms
+
+def processString(textBox):
+    indexValues = []
+    counter = -1 #bc index is zero
+    tempValueHold = -1
+    for char in textBox:
+        counter += 1
+        if char == "*":
+            tempValueHold = counter
+        elif char == "/":
+            indexValues.append(str(tempValueHold) + "," + str(counter))
+            tempValueHold = -1
+        else:
+            None
+    return indexValues
+
+def main(textBox):
+    indexValues = processString(textBox)
+    searchTerms = getSearchValues(indexValues, textBox)
+    getImage.main(searchTerms)
